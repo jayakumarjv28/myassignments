62d65f4d1d317b101bbd2445e06b55120c9cadfa
jayakumarjv28 <117194023+jayakumarjv28@users.noreply.github.com>
2022-11-01 22:31:52 +0530

    Add files via upload
    assignment4

---
+++ Bike.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Bike {
+	public static void main(String[] args) {
+		Car obj=new Car();
+				obj.applybrake();
+		obj.soundHorn();
+	}
+
+}
---
+++ Car.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Car {
+	 public void applybrake() {
+		// TODO Auto-generated method stub
+		System.out.println("print as applied brake"); }
+	 public void soundHorn() {
+		// TODO Auto-generated method stub
+System.out.println("print as soundhorn");
+	}
+		 
+
+	}
+
+
